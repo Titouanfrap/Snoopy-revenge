ecb457ae08c45ff2c54bb59a390acccaa63e2288
Poegens <mathieu.etaix@edu.ece.fr>
2023-10-31 19:04:20 +0100

    Compte à rebours
    Il y'a un bug que je dois corriger et je dois également changer certaines choses pour le rendre plus jolie. Mais sinon il est opérationnel.

---
+++ cmake-build-debug/CMakeFiles/Snoopy_revenge.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snoopy_revenge.dir/main.c.obj"
+  "CMakeFiles/Snoopy_revenge.dir/main.c.obj.d"
+  "Snoopy_revenge.exe"
+  "Snoopy_revenge.exe.manifest"
+  "Snoopy_revenge.pdb"
+  "libSnoopy_revenge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Snoopy_revenge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ temps.h
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define wall 0xFE
+#define Void 0x00
+#define time 0xB1
+
+
+void affichagmatrice (char matrice[10][20]){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<20; j++){
+            printf("%c",matrice[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void tableau(char matrice[10][20]){
+    for (int i=0; i<10; i++){
+        for(int j=0;j<20; j++){
+            if (i==0 || i==9 || j==0 || j==19){
+                matrice[i][j]=wall;
+            }else {
+                matrice[i][j]=Void;
+            }
+        }
+    }
+}
+
+struct Time{
+    int valeur;
+    int x; int directx;
+    int y; int directy;
+};
+
+void comptearebours(){
+    char matrice[10][20];
+    tableau(matrice);
+    struct Time t;
+    t.valeur=120;
+    t.x=0;
+    t.y=0;
+    t.directx=0;
+    t.directy=1;
+
+    while (t.valeur>=0){
+        matrice[t.x][t.y]= time;
+        affichagmatrice(matrice);
+        sleep(2);
+        system("cls");
+
+        t.x +=t.directx;
+        t.y +=t.directy;
+
+        if (t.x==0 && t.y==0){
+            t.directx=0;
+            t.directy=1;
+        }else if (t.x==0 && t.y==19){
+            t.directx=1;
+            t.directy=0;
+        }else if (t.x==9 && t.y==19){
+            t.directx=0;
+            t.directy=-1;
+        }else if (t.x==9 && t.y==0){
+            t.directx=-1;
+            t.directy=0;
+        }else if (t.x==1 && t.y==0){
+            break;
+        }
+    }
+}
+
+
+/*int main(){
+    comptearebours();
+    return 0;
+}
+ comptearebours permet d'executer tout le programme. Mais pour le moment il y'a une erreur
+ que je vais corriger demain ou dans la soirée si j'ai la motivation.
+ */
