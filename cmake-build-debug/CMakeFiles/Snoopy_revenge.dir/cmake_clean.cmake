fee3561e31c49f96997c35f415bda60ad823acb3
marc269 <marcvergnole13@gmail.com>
2023-11-08 21:28:27 +0100

    deplacement

---
+++ cmake-build-debug/CMakeFiles/Snoopy_revenge.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snoopy_revenge.dir/Terrain.c.obj"
+  "CMakeFiles/Snoopy_revenge.dir/Terrain.c.obj.d"
+  "CMakeFiles/Snoopy_revenge.dir/balle.c.obj"
+  "CMakeFiles/Snoopy_revenge.dir/balle.c.obj.d"
+  "CMakeFiles/Snoopy_revenge.dir/main.c.obj"
+  "CMakeFiles/Snoopy_revenge.dir/main.c.obj.d"
+  "CMakeFiles/Snoopy_revenge.dir/temps.c.obj"
+  "CMakeFiles/Snoopy_revenge.dir/temps.c.obj.d"
+  "Snoopy_revenge.exe"
+  "Snoopy_revenge.exe.manifest"
+  "Snoopy_revenge.pdb"
+  "libSnoopy_revenge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Snoopy_revenge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
