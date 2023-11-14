97cf6f5f27e98a4785e8e31300c2e2ef10b44550
BalthaBF <balthazar.bertrandfoliot@edu.ece.fr>
2023-11-14 13:49:33 +0100

    blocs
    Mise en place des blocs

--- snoopy.c
+++ snoopy.c
@@ -10,27 +10,32 @@ void deplacement(char tab[14][24], char *key, Snoopy *snoopy, int *compteur) {
 
     switch (*key) {
         case 'z':
+            if (tab[snoopy->x-1][snoopy->y] != 1){
             if (snoopy->x > 2) {
                 snoopy->x--;
-            }
+            }}
             break;
         case 'q':
+            if (tab[snoopy->x][snoopy->y-1] != 1){
             if (snoopy->y > 2) {
                 snoopy->y--;
-            }
+            }}
             break;
         case 's':
+            if (tab[snoopy->x+1][snoopy->y] != 1){
             if (snoopy->x < 11) {
                 snoopy->x++;
-            }
+            }}
             break;
         case 'd':
-            if (snoopy->y < 21) {
-                snoopy->y++;
+            if (tab[snoopy->x][snoopy->y+1] != 1) {
+                if (snoopy->y < 21) {
+                    snoopy->y++;
+                }
             }
             break;
-    }
 
+    }
     if (tab[snoopy->x][snoopy->y] == 11) {
         (*compteur)++;
     }
