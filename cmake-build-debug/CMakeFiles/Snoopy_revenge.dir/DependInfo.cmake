b065e2fe427477ae60aa558432d8ed5d003e2477
Poegens <mathieu.etaix@edu.ece.fr>
2023-11-14 20:43:29 +0100

    correction GAME OVER

---
+++ cmake-build-debug/CMakeFiles/Snoopy_revenge.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/FinNiveaux.c" "CMakeFiles/Snoopy_revenge.dir/FinNiveaux.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/FinNiveaux.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/balle.c" "CMakeFiles/Snoopy_revenge.dir/balle.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/balle.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/blocs.c" "CMakeFiles/Snoopy_revenge.dir/blocs.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/blocs.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/main.c" "CMakeFiles/Snoopy_revenge.dir/main.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/main.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/menu.c" "CMakeFiles/Snoopy_revenge.dir/menu.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/menu.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/snoopy.c" "CMakeFiles/Snoopy_revenge.dir/snoopy.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/snoopy.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/temps.c" "CMakeFiles/Snoopy_revenge.dir/temps.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/temps.c.obj.d"
+  "C:/Users/etaix/OneDrive/Documents/GitHub/Snoopy-revenge/terrain.c" "CMakeFiles/Snoopy_revenge.dir/terrain.c.obj" "gcc" "CMakeFiles/Snoopy_revenge.dir/terrain.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
