6d72185183456519417f75db9ae787184d19dddf
Titouanfrap <148688173+Titouanfrap@users.noreply.github.com>
2023-11-20 14:05:50 +0100

    changelent niveau 2

--- cmake-build-debug/CMakeFiles/Snoopy_revenge.dir/Lvl1.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Snoopy_revenge.dir/Lvl1.c.obj: \
- C:\Users\balth\OneDrive\Documents\GitHub\Snoopy-revenge\Lvl1.c
--- snoopy.c
+++ snoopy.c
@@ -16,9 +16,10 @@ void deplacement(char tab[14][24], char *key, Snoopy *snoopy, int *compteur) {
                         if (tab[snoopy->x-1][snoopy->y] != 6 ){
                             if (tab[snoopy->x-1][snoopy->y] != 4 ){
                                 if (tab[snoopy->x-1][snoopy->y] != 3 ){
+                                    if (tab[snoopy->x-1][snoopy->y] != 13) {
             if (snoopy->x > 2) {
                 snoopy->x--;
-            }}}}}}}
+            }}}}}}}}
             break;
         case 'q':
             if (tab[snoopy->x][snoopy->y-1] != 2){
@@ -27,6 +28,7 @@ void deplacement(char tab[14][24], char *key, Snoopy *snoopy, int *compteur) {
                         if (tab[snoopy->x][snoopy->y-1] != 7){
                             if (tab[snoopy->x][snoopy->y-1] != 4){
                                 if (tab[snoopy->x][snoopy->y-1] != 3){
+                                    if (tab[snoopy->x-1][snoopy->y] != 13) {
             if (snoopy->y > 2) {
                 snoopy->y--;
             }}}}}}}
@@ -38,6 +40,8 @@ void deplacement(char tab[14][24], char *key, Snoopy *snoopy, int *compteur) {
                         if (tab[snoopy->x+1][snoopy->y] != 2){
                             if (tab[snoopy->x+1][snoopy->y] != 3){
                                 if (tab[snoopy->x+1][snoopy->y] != 4){
+                                    if (tab[snoopy->x-1][snoopy->y] != 13) {
+                                }
             if (snoopy->x < 11) {
                 snoopy->x++;
             }}}}}}}
@@ -49,10 +53,12 @@ void deplacement(char tab[14][24], char *key, Snoopy *snoopy, int *compteur) {
                         if (tab[snoopy->x][snoopy->y+1] != 4) {
                             if (tab[snoopy->x][snoopy->y+1] != 7) {
                                 if (tab[snoopy->x][snoopy->y+1] != 6) {
+                                    if (tab[snoopy->x-1][snoopy->y] != 13) {
+                                }
                 if (snoopy->y < 21) {
                     snoopy->y++;
                 }
-            }}}}}}
+            }}}}}}}
             break;
         case 'a':
             if (tab[snoopy->x+1][snoopy->y] == 13) {
