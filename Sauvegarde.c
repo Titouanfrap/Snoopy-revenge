cda6926caaf64ed8bfbea69d868157e209c7a242
BalthaBF <balthazar.bertrandfoliot@edu.ece.fr>
2023-12-03 20:09:53 +0100

    fin commentaires

--- Sauvegarde.c
+++ Sauvegarde.c
@@ -2,10 +2,10 @@
 #include "Sauvegarde.h"
 #include "unistd.h"
 
-
+// la fonction saveGame permet d'écrire chacun des parametres en binaire dans un fichier
 void saveGame(Snoopy *snoopy, Balle *balle, Time *t, int *oiseaux, int *vies, int *scoretempo, char tab[14][24], int *level) {
-    FILE *file = fopen("savegame.dat", "wb");
-    if (file != NULL) {
+    FILE *file = fopen("savegame.dat", "wb");//on ouvre en écriture binaire
+    if (file != NULL) {// condition qui garantit que l'on écrit bien dans la fichier
         fwrite(snoopy, sizeof(Snoopy), 1, file);
         fwrite(balle, sizeof(Balle), 1, file);
         fwrite(oiseaux, sizeof(int), 1, file);
@@ -16,18 +16,18 @@ void saveGame(Snoopy *snoopy, Balle *balle, Time *t, int *oiseaux, int *vies, in
 
         for (int i=0; i<14; i++){
             for (int j=0; j<24; j++){
-                fwrite(&tab[i][j], sizeof(int), 1, file);
+                fwrite(&tab[i][j], sizeof(int), 1, file); // on fait une boucle pour ecrire toutes les cases de la matrice
             }}
 
-        fclose(file);
-    } else {
+        fclose(file);// on ferme le fichier
+    } else {// si jamais on a file=NULL alors on affiche une erreur lors de la sauvegarde
         printf("Erreur lors de la sauvegarde du jeu.\n");
     }
 }
 
-
+//cette fonction permet de charger la sauvegarde en lisant et assignant toutes les valeurs issues du fichier binaire
 void loadGame(Snoopy *snoopy, Balle *balle, Time *t, int *oiseaux, int *vies, int *scoretempo, char tab[14][24], int *level, char *tempo, char *tempos, int *scorefinal) {
-    FILE *file = fopen("savegame.dat", "rb");
+    FILE *file = fopen("savegame.dat", "rb");// on ouvre en lecture binaire
     if (file != NULL) {
         fread(snoopy, sizeof(Snoopy), 1, file);
         fread(balle, sizeof(Balle), 1, file);
@@ -43,9 +43,9 @@ void loadGame(Snoopy *snoopy, Balle *balle, Time *t, int *oiseaux, int *vies, in
 
 
         sleep(1);
-        terrain(tab);
+        terrain(tab);//on affiche toutes les valeurs enregistrées une fois assignées
 
-        fclose(file);
+        fclose(file);// on ferme le fichier
     } else {
         printf("Erreur lors du chargement de la sauvegarde");
     }
